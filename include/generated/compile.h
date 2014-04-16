064cf243ef853d8835734eb2ded278f281b6619e
abc <abc@abc-To-be-filled-by-O-E-M.(none)>
2014-04-16 17:20:31 -0400

    patch

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 284 */
+/* This file is auto generated, version 285 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#284 SMP PREEMPT Fri Nov 22 14:05:25 CST 2013"
-#define LINUX_COMPILE_BY "rp210"
-#define LINUX_COMPILE_HOST "limixiong"
+#define UTS_VERSION "#285 SMP PREEMPT Wed Apr 16 17:20:13 EDT 2014"
+#define LINUX_COMPILE_BY "abc"
+#define LINUX_COMPILE_HOST "abc-To-be-filled-by-O-E-M"
 #define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-67) "
